af572c1f07dc4925ab8e4ca184f94d029a711524
Victoria Smaranda <victoria.elena01@yahoo.com>
2023-12-21 12:24:13 +0200

    feat: Created view components, breadcrumbs component and show page

---
+++ job_board/resources/views/job/show.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+<x-breadcrumbs :$job class="mb-4"
+:links="['Jobs' => route('jobs.index'), $job->title => '#']">
+
+</x-breadcrumbs>
+    <x-job-card :$job>
+
+    </x-job-card>
+</x-layout>
