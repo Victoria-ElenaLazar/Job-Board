75c2409d823dd08a70abafa04ca0a4e5823afb3e
Victoria Smaranda <victoria.elena01@yahoo.com>
2024-01-10 14:47:22 +0200

    feat: Created user applications page to see the job applications until now, displaying success messages. Implemented middleware auth. Implemented CV uploading space to submit the application

--- job_board/app/Models/User.php
+++ job_board/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,6 +13,10 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    public function jobApplications(): HasMany
+    {
+        return $this->hasMany(JobApplication::class);
+    }
 
 
     /**
--- job_board/routes/web.php
+++ job_board/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\MyJobApplicationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +24,13 @@ Route::resource('jobs', JobController::class)
 Route::get('login', fn() => to_route('auth.create'))->name('login');
 Route::resource('auth', AuthController::class)
     ->only(['create', 'store']);
+Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
+Route::delete('auth', [AuthController::class, 'destroy'])
+    ->name('auth.destroy');
+
+Route::middleware('auth')->group(function(){
+   Route::resource('job.application', JobApplicationController::class)
+   ->only(['create', 'store']);
+    Route::resource('my-job-applications', MyJobApplicationController::class)
+        ->only(['index', 'destroy']);
+});
